d349a8765e1b6f04350a2257c4d4bcbc0616730e
Divyansh Rai <raidiv.3608@gmail.com>
2026-07-19 02:17:47 +0530

    Render tasks dynamically using DOM

--- js/script.js
+++ js/script.js
@@ -13,3 +13,60 @@ console.log(taskForm);
 console.log(taskNameInput);
 console.log(prioritySelect);
 console.log(dueDateInput);
+
+let tasks=[];
+function renderTasks() {
+
+    taskContainer.innerHTML = "";
+
+    tasks.forEach(function(task){
+
+        const taskCard = document.createElement("div");
+
+        taskCard.classList.add("task-card");
+
+        taskCard.innerHTML = `
+    <h3>${task.name}</h3>
+
+    <p><strong>Priority:</strong> ${task.priority}</p>
+
+    <p><strong>Due Date:</strong> ${task.dueDate}</p>
+
+    <p><strong>Status:</strong> ${
+        task.completed ? "Completed ✅" : "Pending ⏳"
+    }</p>
+`;
+
+        taskContainer.appendChild(taskCard);
+
+    });
+
+}
+taskForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const taskName = taskNameInput.value;
+    const priority = prioritySelect.value;
+    const dueDate = dueDateInput.value;
+
+    
+    const task = {
+
+        id: Date.now(),
+
+        name: taskName,
+
+        priority: priority,
+
+        dueDate: dueDate,
+
+        completed: false
+
+    };
+
+    tasks.push(task);
+
+    renderTasks();
+
+});
